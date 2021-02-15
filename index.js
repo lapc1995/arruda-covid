72233e888554d1468830ba999d978f9673f66cd1
Luis Carvalho <lapc19@hotmail.com>
2021-02-15 22:56:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const got = require('got');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const url = "http://www.oestecim.pt/CustomPages/ShowPage.aspx?pageid=481bf11b-80a2-4b10-ad99-da50ba450b36";
+
+(async () => {
+    const response = await got(url);
+    const dom = new JSDOM(response.body);
+    const table = [...dom.window.document.getElementById('content-zone').querySelectorAll('tbody')[1].querySelectorAll('tr')];
+
+    const data = JSON.parse(fs.readFileSync('data.json'));
+    
+    //Get date
+    const translateMonth = {
+        "janeiro" : "january",
+        "fevereiro" : "february",
+        "março" : "march",
+        "abril" : "april",
+        "maio" : "may",
+        "junho" : "june",
+        "julho" : "july",
+        "agosto" : "august",
+        "setembro" : "september",
+        "outubro" : "october",
+        "novembro" : "november",
+        "dezembro" : "december"
+    }
+    const text = table[1].querySelectorAll('span')[2].textContent.replace(",", "").split(" ");
+    const day = text[5];
+    const month = translateMonth[text[7]];
+    const year = table[1].querySelectorAll('span')[1].textContent.split(" ")[1]
+    const timestamp = Date.parse(`${month} ${day}, ${year}`);
+
+    if(data.length === 0 || data[data.length-1].timestamp != timestamp) {
+ 
+        //Get number of cases, number os active cases, number of recuperados, number of death
+        const row = [...table[5].querySelectorAll('td')];
+        const result = [];
+        row.forEach(e => { 
+            const temp = e.querySelectorAll('span')[0];
+            if(temp != undefined)
+                result.push(temp.textContent)    
+        })
+
+        const formatted = {
+            timestamp,
+            "place" : result[0],
+            "total" : Number(result[1]),
+            "active" : Number(result[2]),
+            "recovered" : Number(result[3]),
+            "deaths" : Number(result[4])
+        }
+
+        data.push(formatted);
+        fs.writeFileSync('data.json', JSON.stringify(data));
+
+        console.log(result);
+    }
+
+})();
